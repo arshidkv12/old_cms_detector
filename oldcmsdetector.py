7ab7d04f4ac2ed050c324132f7e856a8383b525b
arshidkv12 <arshidkv12@gmail.com>
2015-11-27 22:46:27 +0530

    Create oldcmsdetector.py

---
+++ oldcmsdetector.py
@@ -0,0 +1,43 @@
+import os
+import pprint
+
+print "-------------------------------------------------"
+print "-------------------------------------------------"
+print "--------- WordPress Current Version ------------"
+print "-------------------------------------------------"
+print "-------------------- 4.3.1 ----------------------"
+print "-------------------------------------------------"
+print "----------------- STARTED -----------------------"
+
+# Author: Arshid 
+# www.cipherCoin.com
+#email: arshidkv12@gmail.com
+#
+
+file_name = open("oldCMS.txt", "a")
+
+for dirname, dirnames, filenames in os.walk('.'):
+
+    for subdirname in dirnames:
+
+        if "wp-include" in subdirname:
+
+            path = os.path.join(dirname, subdirname)+"/version.php"
+            for line in open(path) :
+                if "wp_version =" in line:
+                    i = line.strip("$wp_version = '")
+                    tmp_var_k = i.replace("';",'')
+                    k = tmp_var_k.replace('\n','')
+                    k = k.replace('.','')
+                   
+                    j = float(k)
+                    if j < 431:
+                        file_name.write("Old version:"+ tmp_var_k+"\n" + path + "\n"+ "\n")
+                        print "Old version:"+ tmp_var_k+"\n" + path + "\n"+ "\n"
+
+
+print "--------- COMPLETED ------------"
+                     
+                    
+                         
+                    
